9ee2ad011a9099322a6f5bd216c53a2374e692ce
sharan532 <64846936+sharan532@users.noreply.github.com>
2026-02-24 07:30:29 +0000

    separated react and neeed to install spring in backend

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Hooks Practice</h1>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App
---
+++ frontend-react/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <h2>Counter</h2>
+//       <p>Count: {count}</p>
+
+//       <button onClick={() => setCount(count + 1)}>+</button>
+//       <button onClick={() => setCount(count - 1)}>-</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
